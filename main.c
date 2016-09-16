410f1f31b2cb54e7cf539551422dcec482a3de16
Sergey-Volchkov <seriyvlastelin@gmail.com>
2016-09-16 18:04:43 +0400

    Update main.c

--- main.c
+++ main.c
@@ -12,9 +12,9 @@ int main()
     F=sin(28*pow(a,2)-57*a*x+14*pow(x,2));
     Y=log(-27*pow(a,2)+24*a*x+35*pow(x,2)+1);
 
-    printf("%1.3f\n", G);
-    printf("%1.3f\n", F);
-    printf("%1.3f\n", Y);
+    printf("%.3f\n", G);
+    printf("%.3f\n", F);
+    printf("%.3f\n", Y);
 
 
     return 0;
